675a51dab944430083983b3d7bb96bded062306f
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2021-03-14 14:37:15 +0300

    first commit

---
+++ pkg/handlers/handlers.go
@@ -0,0 +1,51 @@
+package handlers
+
+import (
+	"github.com/RomanAVolodin/go-booking/pkg/config"
+	"github.com/RomanAVolodin/go-booking/pkg/models"
+	"github.com/RomanAVolodin/go-booking/pkg/render"
+	"net/http"
+)
+
+
+// Repo the repository useed by the handlers
+var Repo *Repository
+
+// Repository is the repository tyoe
+type Repository struct {
+	App *config.AppConfig
+}
+
+// NewRepo creates new repository
+func NewRepo(a *config.AppConfig) *Repository {
+	return &Repository{
+		App: a,
+	}
+}
+
+// NewHandlers sets the repository for the handlers
+func NewHandlers(r *Repository) {
+	Repo = r
+}
+
+// Home is the home page handler
+func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIp)
+
+	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
+}
+
+// About is the bout page handler
+func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
+	stringMap := make(map[string]string)
+	stringMap["test"] = "Hello"
+
+
+	remoteIp := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIp
+
+	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
+		StringMap: stringMap,
+	})
+}
