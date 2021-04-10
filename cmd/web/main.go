c74549f8bf0c9a81dfad02db4094a69293098866
Roman Volodin <32717824+RomanAVolodin@users.noreply.github.com>
2021-04-10 19:34:27 +0300

    tests begins, proceed to 68

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,7 +20,23 @@ var session *scs.SessionManager
 
 // main is the main function
 func main() {
+	err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	fmt.Println(fmt.Sprintf("starting Application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
+}
+
+func run() error {
 	// what am i going to put into sessions
 	gob.Register(models.Reservation{})
 
@@ -37,6 +53,7 @@ func main() {
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache", err)
+		return err
 	}
 
 	app.TemplateCache = tc
@@ -46,13 +63,5 @@ func main() {
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 
-	fmt.Println(fmt.Sprintf("starting Application on port %s", portNumber))
-
-	srv := &http.Server{
-		Addr:    portNumber,
-		Handler: routes(&app),
-	}
-
-	err = srv.ListenAndServe()
-	log.Fatal(err)
+	return nil
 }
